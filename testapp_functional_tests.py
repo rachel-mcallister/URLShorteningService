0c76d0d6b5c20d2e1f87c1b58fe502b75e08be85
40199551 <rmcallister13@qub.ac.uk>
2022-09-07 12:47:15 +0100

    reformat

--- testapp_functional_tests.py
+++ testapp_functional_tests.py
@@ -15,18 +15,17 @@ class TestAPI(unittest.TestCase):
         assert b"shorten URL" in response.data
         assert b"Enter a URL:" in response.data
 
-
     def test_home_page_post_with_url_that_does_not_exist(self):
         long_url = 'https://blog.miguelgrinberg.com/post/how-to-write-unit-tests-in-python-part-3-web-applications'
         response = self.app.post('/', data={
-            'url': long_url },
+            'url': long_url},
                                  follow_redirects=True)
         assert response.status_code == 200
         assert response.request.path == '/'
         assert b"http://127.0.0.1:5000/" in response.data
         assert b"BACK TO HOME" in response.data
 
-        #check its added to db
+        # check its added to db
         db = "MyUrlsDB.db"
         connection = sqlite3.connect(db, check_same_thread=False)
         cursor = connection.cursor()
@@ -34,8 +33,6 @@ class TestAPI(unittest.TestCase):
 
         results = cursor.fetchall()
         assert len(results) != 0
-
-
 
     def test_home_page_post_with_url_that_does_exist(self):
         existing_URL = 'https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/'
@@ -55,14 +52,12 @@ class TestAPI(unittest.TestCase):
         assert response.data.decode("utf-8") == 'URL does not exist'
         assert b"URL does not exist" in response.data
 
-
     def test_short_url_that_does_exist(self):
         response = self.app.get('/Y1Smd4cI')
         # moved to the next page
         assert response.status_code == 302
         assert b"Redirecting" in response.data
         assert b"https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/" in response.data
-
 
     def test_get_AllUrls(self):
         response = self.app.get('/getAllUrls')
